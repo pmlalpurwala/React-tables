f1771a332f80637dd4ae71effa46b947f960e1c2
pmlalpurwala <58504600+pmlalpurwala@users.noreply.github.com>
2021-03-24 16:23:44 +0530

    Update Screen1.js

--- src/components/Screen1.js
+++ src/components/Screen1.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+//import { useState } from "react";
 import Persondata from "./Persondata";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 const Screen1 = ({ person, setPerson, currentScreen, setCurrentScreen }) => {
